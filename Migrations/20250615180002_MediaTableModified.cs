644c1f07369ece9ad0cda8084180fe3162547545
Omair Bin Abdur Rahman <36977128+Omair-Rahman@users.noreply.github.com>
2025-06-16 22:53:05 +0600

    modified

---
+++ Migrations/20250615180002_MediaTableModified.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class MediaTableModified : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FullPath",
+                table: "MediaContents",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PreviewPath",
+                table: "MediaContents",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullPath",
+                table: "MediaContents");
+
+            migrationBuilder.DropColumn(
+                name: "PreviewPath",
+                table: "MediaContents");
+        }
+    }
+}
